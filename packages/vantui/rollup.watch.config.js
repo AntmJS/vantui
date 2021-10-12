f6a77fcdb6ea57b62079cd6269d7fa6fdfadad2d
三少 <sanshao@dian.so>
2021-10-12 15:33:15 +0800

    fix: test

--- packages/vantui/rollup.watch.config.js
+++ packages/vantui/rollup.watch.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { resolve, join } = require('path')
+const { resolve, join, dirname } = require('path')
 const { getBabelOutputPlugin } = require('@rollup/plugin-babel')
 const json = require('@rollup/plugin-json')
 const image = require('@rollup/plugin-image')
@@ -8,6 +8,7 @@ const typescript = require('@rollup/plugin-typescript')
 const commonjs = require('@rollup/plugin-commonjs')
 const styles = require('rollup-plugin-styles')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const apis = require('@tarojs/taro-h5/dist/taroApis')
 const cwd = process.cwd()
 
 const config = {
@@ -90,7 +91,66 @@ const h5Config = {
       skipLibCheck: true,
     }),
     getBabelOutputPlugin({
+      exclude: /node_modules/,
       configFile: resolve(__dirname, '../../babel.config.js'),
+    }),
+    getBabelOutputPlugin({
+      include: /node_modules\/@tarojs(.+?)\.[tj]sx?$/i,
+      presets: [
+        [
+          '@antmjs/babel-preset',
+          {
+            presets: {
+              env: {
+                debug: false,
+
+                /**
+                 * false: 不处理polyfill，自己手动引入【全量】
+                 * usage: 按需加载 polyfill，且不需要手动引入【按需】
+                 * entry: 必须手动引入，但会根据设置的目标环境全量导入【按环境全量】
+                 * 注：在 Babel 7.4.0 之后的版本，Babel官方明确建议了不再使用 @babel/polyfill ，建议使用 core-js/stable 和 regenerator-runtime/runtime。本包已经安装了core-js、@babel/plugin-transform-runtime和@babel/runtime，所以选择false或者entry选项的只需要在主文件顶部引入core-js即可
+                 */
+                useBuiltIns: false,
+                corejs: false,
+                modules: false, // 对es6的模块文件不做转译，以便使用tree shaking、sideEffects等
+              },
+              react: {
+                runtime: 'automatic',
+              },
+              typescript: {
+                isTSX: true,
+                jsxPragma: 'React',
+                allExtensions: true,
+                allowNamespaces: true,
+              },
+            },
+            decorators: {
+              legacy: false,
+              decoratorsBeforeExport: false,
+            },
+            classProperties: {
+              loose: false,
+            },
+            runtime: {
+              absoluteRuntime: dirname(
+                require.resolve('@babel/runtime-corejs3/package.json'),
+              ),
+              version: require('@babel/runtime-corejs3/package.json').version,
+              corejs: false,
+              helpers: true, // 使用到@babel/runtime
+              regenerator: true, // 使用到@babel/runtime
+              useESModules: false,
+            },
+            exclude: [/@babel[/|\\\\]runtime/, /core-js/],
+          },
+        ],
+      ],
+      plugins: [
+        [
+          require('babel-plugin-transform-taroapi'),
+          { packageName: '@tarojs/taro', apis },
+        ], // taro可以加，tree-shaking用
+      ],
     }),
     image(),
     styles(),
