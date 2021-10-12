deebcd8a554bcc163aac6c93fa6d81064d732a91
三少 <sanshao@dian.so>
2021-10-12 13:44:37 +0800

    feat: 添加React的支持

--- packages/vantui/rollup.watch.config.js
+++ packages/vantui/rollup.watch.config.js
@@ -6,6 +6,7 @@ const image = require('@rollup/plugin-image')
 const copy = require('rollup-plugin-copy-watch')
 const typescript = require('@rollup/plugin-typescript')
 const commonjs = require('@rollup/plugin-commonjs')
+const styles = require('rollup-plugin-styles')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
 const cwd = process.cwd()
 
@@ -56,4 +57,44 @@ const config = {
   ],
 }
 
-module.exports = [config]
+const h5Config = {
+  input: join(cwd, 'src/index.ts'),
+  output: [
+    {
+      file: join(cwd, 'dist/h5.js'),
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      sourcemap: true,
+      format: 'esm',
+      file: join(cwd, 'dist/h5.esm.js'),
+    },
+  ],
+  external: ['@babel/runtime-corejs3', 'react', 'react-dom'],
+  plugins: [
+    commonjs({
+      include: /\/node_modules\//,
+    }),
+    nodeResolve({
+      extensions: ['.mjs', '.js', '.jsx', '.tsx', '.ts', '.vue'],
+      mainFields: ['main:h5', 'browser', 'module', 'jsnext:main', 'main'],
+      customResolveOptions: {
+        moduleDirectories: ['node_modules'],
+      },
+    }),
+    json(),
+    typescript({
+      tsconfig: resolve(__dirname, '../../tsconfig.json'),
+      module: 'esnext',
+      skipLibCheck: true,
+    }),
+    getBabelOutputPlugin({
+      configFile: resolve(__dirname, '../../babel.config.js'),
+    }),
+    image(),
+    styles(),
+  ],
+}
+
+module.exports = [config, h5Config]
