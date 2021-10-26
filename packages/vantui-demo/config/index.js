8b345d4eebd67a2d9547004768b7d1e71786a58c
langen <langen@dian.so>
2021-10-26 16:24:15 +0800

    feat: 增加doc打包

--- packages/vantui-demo/config/index.js
+++ packages/vantui-demo/config/index.js
@@ -3,7 +3,61 @@ const npath = require('path')
 const pkg = require('../package.json')
 const miniChain = require('./webpack/miniChain')
 const h5Chain = require('./webpack/h5Chain')
-
+const h5Touter = [
+  'pages/dashboard/index',
+  'pages/action-sheet/index',
+  'pages/button/index',
+  'pages/card/index',
+  'pages/cell/index',
+  'pages/col/index',
+  'pages/count-down/index',
+  'pages/dialog/index',
+  'pages/field/index',
+  'pages/icon/index',
+  'pages/image/index',
+  'pages/loading/index',
+  'pages/nav-bar/index',
+  'pages/notice-bar/index',
+  'pages/notify/index',
+  // 'pages/panel/index',
+  'pages/popup/index',
+  'pages/progress/index',
+  'pages/stepper/index',
+  'pages/steps/index',
+  'pages/sticky/index',
+  'pages/switch/index',
+  'pages/search/index',
+  'pages/slider/index',
+  'pages/sidebar/index',
+  'pages/tab/index',
+  'pages/tabbar/index',
+  'pages/tag/index',
+  'pages/toast/index',
+  'pages/transition/index',
+  'pages/tree-select/index',
+  'pages/area/index',
+  'pages/submit-bar/index',
+  'pages/radio/index',
+  'pages/checkbox/index',
+  'pages/goods-action/index',
+  'pages/swipe-cell/index',
+  'pages/uploader/index',
+  'pages/datetime-picker/index',
+  'pages/rate/index',
+  'pages/collapse/index',
+  'pages/picker/index',
+  'pages/overlay/index',
+  'pages/circle/index',
+  'pages/grid/index',
+  'pages/dropdown-menu/index',
+  'pages/index-bar/index',
+  'pages/skeleton/index',
+  'pages/divider/index',
+  'pages/empty/index',
+  'pages/calendar/index',
+  'pages/share-sheet/index',
+  'pages/config-provider/index',
+]
 function getVersion() {
   function fillZero(value) {
     return value < 10 ? `0${value}` : `${value}`
@@ -90,11 +144,18 @@ let config = {
           .publicPath(publicPath.replace('VERSION', version))
       }
       if (process.env.WATCHING === 'true') {
-        chain.output.publicPath(`/vantui`)
+        chain.output.publicPath(`/`)
       }
     },
     router: {
       mode: 'hash',
+      // 'pages/empty/index'
+      customRoutes: process.env.DOC
+        ? h5Touter.reduce((p, n) => {
+            p[n] = n.replace(/pages\/([\w-]+)\/index/, '$1')
+            return p
+          }, {})
+        : '',
     },
     devServer: {
       port: 10086,
