d4034ff6135c117173f4d34b23e5beb7ed7a4a2e
zuolung <zuolung@126.com>
2021-10-27 15:41:39 +0800

    Updates

---
+++ assets/js/chunk/71.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{"228":function(n,t,o){"use strict";o.r(t),o.d(t,"taro_block_core",(function(){return c}));var e=o(45),c=function(){function r(n){Object(e.g)(this,n)}return r.prototype.render=function(){return Object(e.e)(e.a,null)},r}()}}]);
