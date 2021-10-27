471dbdb547d83ccd87cfbe5f37bd09d91a007d15
zuolung <zuolung@126.com>
2021-10-27 15:54:48 +0800

    Updates

--- assets/js/chunk/81.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[81],{"241":function(n,o,r){"use strict";r.r(o),r.d(o,"taro_movable_area_core",(function(){return c}));var t=r(45),c=function(){function e(n){Object(t.g)(this,n)}return e.prototype.componentDidLoad=function(){console.error("H5 暂不支持 MovableArea 组件！")},e.prototype.render=function(){return Object(t.e)(t.a,null)},e}()}}]);
--- assets/js/chunk/83.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[83],{"244":function(n,o,e){"use strict";e.r(o),e.d(o,"taro_open_data_core",(function(){return c}));var r=e(45),c=function(){function t(n){Object(r.g)(this,n)}return t.prototype.componentDidLoad=function(){console.error("H5 暂不支持 OpenData 组件！")},t.prototype.render=function(){return Object(r.e)(r.a,null)},t}()}}]);
