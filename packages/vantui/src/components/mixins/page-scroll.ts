065538af2ffa38410ba0ad75a97979dfe571f608
langen <langen@dian.so>
2021-10-28 19:03:50 +0800

    feat: 修复小程序问题

--- packages/vantui/src/components/mixins/page-scroll.ts
+++ packages/vantui/src/components/mixins/page-scroll.ts
@@ -4,22 +4,21 @@ import { usePageScroll as useTaroPageScroll } from '@tarojs/taro'
 export function usePageScroll(scroller: any) {
   useEffect((): any => {
     // 兼容react-ui 弄出来的代码
-    if (process.env.TARO_ENV === 'h5') {
-      const el = document as any
+    if (process.env.TARO_ENV !== 'h5') return
+    const el = document as any
 
-      function listener(event: Event): void {
-        if (!event.target) return
-        const _event = {
-          scrollTop: el.scrollingElement.scrollTop,
-          scrollLeft: el.scrollingElement.scrollLeft,
-        }
+    function listener(event: Event): void {
+      if (!event.target) return
+      const _event = {
+        scrollTop: el.scrollingElement.scrollTop,
+        scrollLeft: el.scrollingElement.scrollLeft,
+      }
 
-        scroller(_event)
-      }
-      el.addEventListener('scroll', listener)
-      return () => {
-        el.removeEventListener('scroll', listener)
-      }
+      scroller(_event)
+    }
+    el.addEventListener('scroll', listener)
+    return () => {
+      el.removeEventListener('scroll', listener)
     }
   })
   useTaroPageScroll((e) => {
