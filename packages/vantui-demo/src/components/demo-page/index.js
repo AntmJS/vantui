44b26a129fcaa604f60938b30721540fc0fe1870
langen <langen@dian.so>
2021-11-05 10:51:34 +0800

    fix: 修复demo 文档跳转问题

--- packages/vantui-demo/src/components/demo-page/index.js
+++ packages/vantui-demo/src/components/demo-page/index.js
@@ -8,6 +8,17 @@ export default function Page(props) {
   // eslint-disable-next-line react/prop-types
   const { title, children } = props
 
+  useEffect(() => {
+    if (process.env.LIBRARY_ENV === 'react') {
+      document.body.scrollTop = document.documentElement.scrollTop = 0
+    } else {
+      pageScrollTo({
+        scrollTop: 0,
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.hash])
+
   return (
     <View className="demo-page">
       {'h5' === process.env.TARO_ENV && (
