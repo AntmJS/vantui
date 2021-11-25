ff76112230604e0cdf94972c4907303cfae98ae1
Chitanda60 <freeyinyutian@gmail.com>
2021-11-25 16:53:19 +0800

    fix: field autosize (#87)
    * fix: 修复field 高度自适应无效 [#85]
    
    * fix: 更新button children的ts类型为非必填 [#86 ]

--- packages/vantui/src/utils/index.ts
+++ packages/vantui/src/utils/index.ts
@@ -1,6 +1,7 @@
 import type ANTMUI from '../../types/normal'
 import { useCallback, useRef, useState } from 'react'
 import { nextTick, createSelectorQuery, SelectorQuery } from '@tarojs/taro'
+import { isObject } from './type'
 
 export function useFadeIn(ref: any) {
   return useCallback(
@@ -149,4 +150,56 @@ export function uuid(len = 8, radix = 16): string {
   }
 
   return value.join('')
+}
+
+type ScrollElement = Element | Window
+type FieldAutosizeConfig = {
+  maxHeight?: number
+  minHeight?: number
+}
+export function getRootScrollTop(): number {
+  return (
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0
+  )
+}
+export function setRootScrollTop(value: number) {
+  setScrollTop(window, value)
+  setScrollTop(document.body, value)
+}
+export function setScrollTop(el: ScrollElement, value: number) {
+  if ('scrollTop' in el) {
+    el.scrollTop = value
+  } else {
+    el.scrollTo(el.scrollX, value)
+  }
+}
+
+export function resizeTextarea(
+  input: HTMLTextAreaElement,
+  autosize: true | FieldAutosizeConfig,
+) {
+  const scrollTop = getRootScrollTop()
+  input.style.height = 'auto'
+  input.style.lineHeight = 'inherit'
+  input.rows = 1
+
+  let height = input.scrollHeight
+  if (isObject(autosize)) {
+    const { maxHeight, minHeight }: any = autosize
+    if (maxHeight !== undefined) {
+      height = Math.min(height, maxHeight)
+    }
+    if (minHeight !== undefined) {
+      height = Math.max(height, minHeight)
+    }
+  }
+
+  if (height) {
+    input.style.height = `${height}px`
+    // https://github.com/youzan/vant/issues/9178
+    setRootScrollTop(scrollTop)
+  }
 }
