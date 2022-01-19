a1e929fc783298347d46235b76fd1209eca70da0
zhixia <zhixia@dian.so>
2022-01-19 11:22:35 +0800

    fix: collapse typescript

--- packages/vantui/types/collapse.d.ts
+++ packages/vantui/types/collapse.d.ts
@@ -2,7 +2,7 @@ import { ComponentClass, ReactNode } from 'react'
 import { ITouchEvent, StandardProps } from '@tarojs/components'
 
 export interface CollapseProps extends StandardProps {
-  value?: Array<string | number>
+  value?: string | Array<string | number>
   accordion?: boolean
   border?: boolean
   children?: Array<ReactNode>
