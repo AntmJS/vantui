591043b315bb5799f1b37d8ea4f482726b21aa40
kongjing@dian.so <apple@AppledeMacBook-Pro-2.local>
2022-02-09 17:36:34 +0800

    feat: git warning

---
+++ antm.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  warning: {
+    monitorFiles: ['package.json', 'antm.config.js'],
+    webhooks: {
+      url: 'https://oapi.dingtalk.com/robot/send?access_token=c2ca61d59a4e22f5d60b8d841494ab5ab570ec190accb16bbee10562092c8fe8',
+    },
+  },
+}
