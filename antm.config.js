2fae68f4f18c7f1b08f2fc3231e36753a8bdd65f
kongjing@dian.so <apple@AppledeMacBook-Pro-2.local>
2022-02-23 14:12:47 +0800

    feat: add warning

--- antm.config.js
+++ antm.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   warning: {
-    monitorFiles: ['package.json', 'antm.config.js'],
+    monitorFiles: [
+      './packages/[!node_modules]**/package.json',
+      './package.json',
+    ],
     webhooks: {
       url: 'https://oapi.dingtalk.com/robot/send?access_token=c2ca61d59a4e22f5d60b8d841494ab5ab570ec190accb16bbee10562092c8fe8',
     },
