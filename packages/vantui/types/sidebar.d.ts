3faaa1f67f6610ef8c25f0e0857b50f6a3c2c5d7
boxcc <lmfmcbo@gmail.com>
2022-03-28 18:14:22 +0800

    Siderbar的onChange事件类型错误 (#207)
    * typings: 规范interface名称

--- packages/vantui/types/sidebar.d.ts
+++ packages/vantui/types/sidebar.d.ts
@@ -1,16 +1,10 @@
 import { ComponentClass } from 'react'
 import { StandardProps } from '@tarojs/components'
 
-interface onChangeEvent {
-  event: {
-    detail?: number
-  }
+interface OnChangeEvent {
+  detail?: number
 }
-interface onChangeEvent {
-  event: {
-    detail?: number
-  }
-}
+
 export interface SidebarItemProps extends StandardProps {
   dot?: boolean
   badge?: string
@@ -18,12 +12,12 @@ export interface SidebarItemProps extends StandardProps {
   title?: string
   disabled?: boolean
   onClick?: (a: any) => void
-  onChange?: (a: onChangeEvent) => void
+  onChange?: (a: OnChangeEvent) => void
   renderTitle?: (a: any) => React.ReactNode
 }
 
 export interface SidebarProps extends StandardProps {
-  onChange?: (a: onChangeEvent) => any
+  onChange?: (a: OnChangeEvent) => any
   activeKey: number
   children: React.ReactNode
 }
