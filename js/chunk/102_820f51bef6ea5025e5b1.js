e746e1adbf6dc3e1407bb35b85a3652eb584860e
kongjing@dian.so <apple@AppledeMacBook-Pro-2.local>
2022-07-08 18:12:18 +0800

    Updates

---
+++ js/chunk/102_820f51bef6ea5025e5b1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[102],{"539":function(n,o,r){"use strict";r.r(o),r.d(o,"taro_movable_area_core",(function(){return c}));var t=r(147),c=function(){function e(n){Object(t.g)(this,n)}return e.prototype.componentDidLoad=function(){console.error("H5 暂不支持 MovableArea 组件！")},e.prototype.render=function(){return Object(t.e)(t.a,null)},e}()}}]);
