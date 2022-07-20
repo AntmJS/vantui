184d0ca26aff25d7548d75c42420e3ce2e975335
Yanghc <hqwlkj@sina.com>
2022-07-20 17:28:53 +0800

    fix: nav-bar 样式问题
    fix: nav-bar 在使用`safeAreaInsetTop`为`false` 并且 `placeholder` 为`true`时，占位组建要比显示的Navbar 高 22 px;

--- packages/vantui/src/nav-bar/index.tsx
+++ packages/vantui/src/nav-bar/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { View } from '@tarojs/components'
 import * as utils from '../wxs/utils'
 import { getSystemInfoSync } from '../common/utils'
@@ -33,11 +33,24 @@ export function NavBar(props: NavBarProps) {
     if (isNaN(statusBarHeight)) statusBarHeight = 22
     setStatusBarHeight(statusBarHeight)
   }, [])
+  
+  const getNavBarStyle = useCallback(()=>{
+    return utils.style([
+          computed.barStyle({
+            zIndex,
+            statusBarHeight,
+            safeAreaInsetTop,
+            height: 50,
+          }) +
+            '; ' +
+            style,
+        ])
+  }, [zIndex, statusBarHeight, safeAreaInsetTop])
 
   return (
     <>
       {fixed && placeholder && (
-        <View style={'height: ' + (50 + statusBarHeight) + 'px;'}></View>
+        <View style={getNavBarStyle()}></View>
       )}
       <View
         className={
@@ -48,16 +61,7 @@ export function NavBar(props: NavBarProps) {
           (border ? 'van-hairline--bottom' : '') +
           ` ${className || ''}`
         }
-        style={utils.style([
-          computed.barStyle({
-            zIndex,
-            statusBarHeight,
-            safeAreaInsetTop,
-            height: 50,
-          }) +
-            '; ' +
-            style,
-        ])}
+        style={getNavBarStyle()}
         {...others}
       >
         <View className="van-nav-bar__content">
