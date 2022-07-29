e309bb03853f095014c4df5b78f0c84d5886071a
Yanghc <hqwlkj@outlook.com>
2022-07-29 17:39:43 +0800

    fix: 添加依赖 (#304)
    * fix: 添加依赖

--- packages/vantui/src/nav-bar/index.tsx
+++ packages/vantui/src/nav-bar/index.tsx
@@ -42,14 +42,14 @@ export function NavBar(props: NavBarProps) {
         safeAreaInsetTop,
         height: 50,
       }) +
-      '; ' +
-      style,
-    ]);
-  }, [zIndex, statusBarHeight, safeAreaInsetTop])
+        '; ' +
+        style,
+    ])
+  }, [zIndex, statusBarHeight, safeAreaInsetTop, style])
 
   return (
     <>
-      {fixed && placeholder && (<View style={getNavBarStyle}></View>)}
+      {fixed && placeholder && <View style={getNavBarStyle}></View>}
       <View
         className={
           utils.bem('nav-bar', {
