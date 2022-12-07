c903a2fd909c4b769bd8d4f6fabd24b8562d32ac
三少 <sanshao@dian.so>
2022-12-07 23:12:40 +0800

    fix: 修复类型错误

--- packages/vantui/src/search/index.tsx
+++ packages/vantui/src/search/index.tsx
@@ -61,7 +61,7 @@ export function Search(props: SearchProps) {
     setTimeout(() => {
       setInnerValue('')
 
-      onCancel?.(e)
+      onCancel?.()
       Object.defineProperty(e, 'detail', {
         value: '',
       })
