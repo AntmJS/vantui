441953f1b76b03a97787f9d8b1a192edc22ce32c
kongjing@dian.so <apple@AppledeMacBook-Pro-2.local>
2022-12-13 16:11:01 +0800

    docs: demo鼠标样式、禁止图片拖拽

--- packages/vantui-demo/src/app.tsx
+++ packages/vantui-demo/src/app.tsx
@@ -1,4 +1,5 @@
 import { usePageNotFound, navigateTo } from '@tarojs/taro'
+import { useEffect } from 'react'
 import './app.less'
 
 export default function App(props: any) {
@@ -7,6 +8,22 @@ export default function App(props: any) {
       url: '/pages/dashboard/index',
     })
   })
+
+  useEffect(() => {
+    // 解决文档展示时图片被拖拽
+    if (process.env.TARO_ENV === 'h5') {
+      setTimeout(() => {
+        const imgs = document.getElementsByTagName('img') || []
+        for (let i = 0; i < imgs.length; i++) {
+          const img = imgs[i]
+          img?.addEventListener('mousedown', function (e) {
+            e.preventDefault()
+          })
+        }
+      }, 2000)
+    }
+  }, [props.children])
+
   return (
     // 在入口组件不会渲染任何内容，但我们可以在这里做类似于状态管理的事情
     props.children
