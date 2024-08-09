7f31f84b9fc7f63529f8953a3da69c8e2a0c3507
MrXiangXvFan <74048960+MrXiangXvFan@users.noreply.github.com>
2024-08-09 09:57:14 +0800

    docs: 新增官方文档点击可以复制icon功能。 (#671)

--- packages/vantui/src/icon/demo/demo4.tsx
+++ packages/vantui/src/icon/demo/demo4.tsx
@@ -1,6 +1,7 @@
 import { Text } from '@tarojs/components'
 import icons from '@vant/icons'
 import { Col, Icon } from '@antmjs/vantui'
+import * as computed from '../wxs'
 
 export default function Demo() {
   return (
@@ -10,6 +11,7 @@ export default function Demo() {
           key={i}
           span="6"
           style={{ paddingTop: '20px', paddingBottom: '20px', height: '100px' }}
+          onClick={() => computed.copyIcon(name)}
         >
           <Icon name={name} size="32px" />
           <Text style={{ textAlign: 'center' }}>{name}</Text>
--- packages/vantui/src/icon/wxs.ts
+++ packages/vantui/src/icon/wxs.ts
@@ -1,3 +1,4 @@
+import Taro from '@tarojs/taro'
 import { style } from '../wxs/style'
 import { addUnit } from '../wxs/add-unit'
 
@@ -33,4 +34,17 @@ function rootStyle(data: any) {
   ])
 }
 
-export { isImage, rootClass, rootStyle }
+//复制icon
+function copyIcon(name: string) {
+  Taro.setClipboardData({
+    data: `<Icon name='${name}' size="32px" />`,
+    success: () => {
+      Taro.showToast({
+        title: '复制成功',
+        icon: 'none',
+      })
+    },
+  })
+}
+
+export { isImage, rootClass, rootStyle, copyIcon }
