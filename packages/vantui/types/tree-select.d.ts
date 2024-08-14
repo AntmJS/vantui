678c503b2780a554c7ae01ad584b7e3fc233a3c6
工边页字 <1096032096@qq.com>
2024-08-14 13:34:00 +0800

    docs: treeSelect组件文档补充

--- packages/vantui/types/tree-select.d.ts
+++ packages/vantui/types/tree-select.d.ts
@@ -1,6 +1,10 @@
 import { FunctionComponent, TouchEvent, ReactNode } from 'react'
 import { ViewProps } from '@tarojs/components'
 export interface TreeSelectProps extends ViewProps {
+  /**
+   * @description 分类显示所需的数据
+   * @default []
+   */
   items?: {
     text: number | string
     badge?: number | string
@@ -12,12 +16,34 @@ export interface TreeSelectProps extends ViewProps {
       disabled?: boolean
     }[]
   }[]
+  /**
+   * @description 右侧选中项的 id，支持传入数组
+   * @default -
+   */
   activeId?: string | number | number[] | string[]
+  /**
+   * @description 	左侧选中项的索引
+   * @default 0
+   */
   mainActiveIndex?: number
-  value?: number
+  /**
+   * @description 高度，默认单位为px
+   * @default 300
+   */
   height?: number | string
+  /**
+   * @description 	右侧项最大选中个数
+   * @default Infinity
+   */
   max?: number
+  /**
+   * @description 	自定义右侧栏选中状态的图标
+   * @default success
+   */
   selectedIcon?: string
+  /**
+   * @description 		点击右侧选择项时触发
+   */
   onClickItem?: (
     event: TouchEvent & {
       detail: {
@@ -27,7 +53,13 @@ export interface TreeSelectProps extends ViewProps {
       }
     },
   ) => void
+  /**
+   * @description 		点击左侧导航时触发
+   */
   onClickNav?: (event: { detail: { index: number } }) => void
+  /**
+   * @description 	自定义右侧的显示内容
+   */
   renderContent?: ReactNode
 }
 declare const TreeSelect: FunctionComponent<TreeSelectProps>
