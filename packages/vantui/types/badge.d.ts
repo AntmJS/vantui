15a7e44a228a39920c2f2f9e80f12d060bc46eec
工边页字 <1096032096@qq.com>
2024-10-05 16:19:02 +0800

    docs: badge文档补全

--- packages/vantui/types/badge.d.ts
+++ packages/vantui/types/badge.d.ts
@@ -2,12 +2,35 @@ import { FunctionComponent, ReactNode } from 'react'
 import { ViewProps } from '@tarojs/components'
 
 export interface BadgeProps extends ViewProps {
+  /**
+   * @description 徽标内容
+   */
   content?: ReactNode
+  /**
+   * @description 颜色
+   * @default #ff3141
+   */
   color?: string
+  /**
+   * @description 是否显示边框
+   * @default false
+   */
   bordered?: boolean
+  /**
+   * @description
+   */
   children?: ReactNode
+  /**
+   * @description 徽标根节点的className
+   */
   wrapperClassName?: string
+  /**
+   * @description 徽标根节点的style
+   */
   wrapperStyle?: React.CSSProperties
+  /**
+   * @description 徽标内容的style
+   */
   style?: React.CSSProperties
 }
 declare const Badge: FunctionComponent<BadgeProps>
