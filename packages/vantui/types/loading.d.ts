3f7ab2298fd54977fd36ad54e76ff766391957fb
MrXiangXvFan <74048960+MrXiangXvFan@users.noreply.github.com>
2025-03-17 16:41:12 +0800

    docs: loading文档补全 (#698)

--- packages/vantui/types/loading.d.ts
+++ packages/vantui/types/loading.d.ts
@@ -1,11 +1,34 @@
 import { FunctionComponent } from 'react'
 import { ViewProps } from '@tarojs/components'
 export interface LoadingProps extends ViewProps {
+  /**
+   * @description 颜色
+   * @default #c8c9cc
+   */
   color?: string
+  /**
+   * @description 是否垂直排列图标和文字内容
+   * @default false
+   */
   vertical?: boolean
+  /**
+   * @description 类型，可选值为 spinner
+   * @default circular
+   */
   type?: 'spinner' | 'circular'
+  /**
+   * @description 加载图标大小，默认单位为px
+   * @default 30px
+   */
   size?: string | number
+  /**
+   * @description 文字大小，默认单位为px
+   * @default 14px
+   */
   textSize?: string | number
+  /**
+   * @description 加载文案
+   */
   children?: React.ReactNode
 }
 
